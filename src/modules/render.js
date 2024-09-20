bcd312dab2fd17427d0a3f426f63e2232aa057ff
vladh <hadyak305246@gmail.com>
2024-09-20 01:17:15 -0600

    Handle project clicks and display added tasks

--- src/modules/render.js
+++ src/modules/render.js
@@ -18,6 +18,24 @@ export function currentProject() {
 };
 
 
-function renderContent(project) {
-  
+function renderContent(projectItem) {
+  const projectID = projectItem.getAttribute("data-id");
+  const projectIndex = projectArr.findIndex(project => project.id === Number(projectID));
+  const project = projectArr[projectIndex];
+
+  const filteredTasks = taskArr.filter(task => task.projectID === project.id);      // Find tasks associated with projects
+
+  // Clear each task UI before displaying required tasks
+  const tasks = document.querySelectorAll(".task");
+  tasks.forEach(task => {
+    task.remove();
+  });
+
+  // Show tasks associated with the project
+  filteredTasks.forEach(task => {
+    displayTask(task.title, task.date, task.priority, task.id);
+  });
+
+  console.log(taskArr);
+  console.log(projectArr);
 };
